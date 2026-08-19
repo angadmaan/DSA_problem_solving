3e456344bd33968cab7c6fb172db83329e7739f1
angadmaan <angadmaancse@gmail.com>
2026-08-19 23:56:13 +0530

    if_else

--- basics/if_else.cpp
+++ basics/if_else.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 using namespace std;
 
-// int main(){
-//     int age;
 
-//     cout << "Enter your age: ";
-//     cin >> age;
-//     if(age >= 18){
-//         cout << "You are adult";
-//     }
-//     else{
-//         cout << "You are not an adult";
-//     }
+// There are three programs in this file (comment out others when running a specific program).
 
-//     return 0;
-// }
+int main(){
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age >= 18){
+        cout << "You are adult";
+    }
+    else{
+        cout << "You are not an adult";
+    }
+
+    return 0;
+}
 
 
 int main(){
@@ -22,27 +25,31 @@ int main(){
     cout << "Enter Your Marks: ";
     cin >> marks;
     
-    if (marks < 0 && marks > 100) {
+    if (marks < 0 || marks > 100) {
         cout << "Invalid marks" << endl;
     }
-    if(marks <= 25){
+    else if(marks <= 25){
         cout << "F" << endl;
     }
-    else if(marks <= 44 && marks >= 25){
+    else if(marks <= 44){
         cout << "E" << endl;
     }
-    else if(marks <= 49 && marks >= 45){
+    else if(marks <= 49){
         cout << "D" << endl;
     }
-    else if(marks >= 50 && marks <= 59){
+    else if(marks <= 59){
         cout << "C" << endl;
     }
-    else if(marks <= 79 && marks >= 60){
+    else if(marks <= 79){
         cout << "B" << endl;
     }
-    else if(marks >= 80 && marks <= 100){
+    else if(marks <= 100){
         cout << "A" << endl;
     }
 
     return 0;
+}
+
+int main(){
+    
 }
