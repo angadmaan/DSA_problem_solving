de2b3537a925bd74b63b43e7b169b85069cc4418
angadmaan <angadmaancse@gmail.com>
2026-08-20 00:11:34 +0530

    if_else

--- basics/if_else.cpp
+++ basics/if_else.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 // There are three programs in this file (comment out others when running a specific program).
 
+
+// Program 1
+
 int main(){
     int age;
 
@@ -19,6 +22,7 @@ int main(){
     return 0;
 }
 
+// Program 2
 
 int main(){
     int marks;
@@ -50,6 +54,27 @@ int main(){
     return 0;
 }
 
+// Program 3
+
 int main(){
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age < 18){
+        cout << "Not eligible for job" << endl;
+    }
+    else if(age <= 57){
+        cout << "You are eligible for job";
+        if(age >= 55){
+            cout << ", retirement soon" << endl;
+        }
+    }
+    else{
+        cout << "Retirement time" << endl;
+    }
     
+
+
+    return 0;
 }
