0f24499d3126f4bc622471668a43583ebce1a6c5
angadmaan <angadmaancse@gmail.com>
2026-08-29 13:19:46 +0530

    bits/stdc++.h

---
+++ bits/stdc++.h
@@ -0,0 +1,46 @@
+#ifndef BITS_STDCPP_H
+#define BITS_STDCPP_H
+
+// Core I/O
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+
+// Containers
+#include <vector>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+// Strings
+#include <string>
+#include <cstring>
+
+// Algorithms
+#include <algorithm>
+#include <numeric>
+#include <functional>
+
+// Math
+#include <cmath>
+#include <complex>
+#include <cstdlib>
+#include <random>
+
+// Utilities
+#include <utility>
+#include <cctype>
+#include <cassert>
+#include <stdexcept>
+#include <limits>
+#include <typeinfo>
+
+using namespace std;
+
+#endif
