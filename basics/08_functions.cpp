c30c3995efb5aff53222a877a047382423857209
angadmaan <angadmaancse@gmail.com>
2026-08-30 19:47:27 +0530

    basics

---
+++ basics/08_functions.cpp
@@ -0,0 +1,16 @@
+// Functions are set of code which performs something for you
+// Functions are used to modularise code
+// Functions are used to increase readability
+// Functions are used to use same code multiple times
+// void -> Does not return anything 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    
+
+    return 0;
+}
