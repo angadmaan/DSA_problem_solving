57bb04e55d30ba42a6313532309ede42ef5c8585
angadmaan <angadmaancse@gmail.com>
2026-09-02 20:22:55 +0530

    basics

---
+++ 01_basics/01_input_output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout <<"Hey Everyone!"<<endl;
+    cout <<"Hey Angad!"<<endl;
+
+    int n;
+    cout <<"Please enter the value of n: ";
+    cin >> n;
+    cout <<"The value of n: " << n <<endl;
+    return 0;
+}
---
+++ 01_basics/07_loops.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// For loop
+
+
+int main() {
+    for (int i = 1; i <= 10; i = i + 1){      // The value of i increases by 1 after 1 iteration.
+        cout << "Angad Singh Maan" << endl;
+    }
+    return 0;
+}
+
+int main() {
+    for (int i = 5; i > 0; i = i - 1){       // The value of i decreases by 1 after 1 iteration.
+        cout << i << endl;
+    }
+    return 0;
+}
+
+
+// Same peice of code with while loop
+
+
+int main(){
+    int i = 1;      // Intialized outside the loop
+    while(i <= 10){     // Condition inside the while loop 
+        cout << i << endl;
+
+        i = i + 1;      // Operation performed in the last step of while loop
+    }
+    return 0;
+}
