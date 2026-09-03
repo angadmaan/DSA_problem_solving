790cbe42eb2af822795ad3de99a261c94d875655
angadmaan <angadmaancse@gmail.com>
2026-09-03 23:10:02 +0530

    pattern_1

--- 03_patterns.md/1.cpp
+++ 03_patterns.md/1.cpp
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+void pattern_1 (int n){
 
-    int i,j;
+     for (int i = 0; i < n; i++){
 
-    for (i = 0; i < 4; i++){
-
-        for(j = 0; j < 4; j++){
+        for(int j = 0; j < n; j++){
 
             cout << "* ";
         }
@@ -15,6 +13,13 @@ int main() {
         cout << endl;
     
     }
+}
 
+int main() {
+
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    pattern_1(n);
     return 0;
 }
