c64dbbe88a670c0d87fe866e1d26c96e2ff3871d
angadmaan <angadmaancse@gmail.com>
2026-09-04 20:23:39 +0530

    pattern_2

---
+++ 03_patterns.md/2.cpp
@@ -0,0 +1,32 @@
+/*
+
+*
+* * 
+* * * 
+* * * *
+* * * * *
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern_2 (int n){
+
+     for (int i = 0; i < n; i++){
+        for (int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    pattern_2(n);
+    return 0;
+}
