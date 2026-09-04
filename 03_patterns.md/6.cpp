33f1b48322f4c26348f3e00527cfa99544a4ffba
angadmaan <angadmaancse@gmail.com>
2026-09-04 21:02:33 +0530

    pattern_6

---
+++ 03_patterns.md/6.cpp
@@ -0,0 +1,33 @@
+/*
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern_1 (int n){
+
+     for (int i = 1; i <= n; i++){
+
+        for(int j = 1; j <= n-i+1; j++){
+
+            cout << j << " ";
+        }
+
+        cout << endl;
+    
+    }
+}
+
+int main() {
+
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    pattern_1(n);
+    return 0;
+}
