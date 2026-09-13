368d4831b9726ca9d1696ffbc6749a5f78d2b696
angadmaan <angadmaancse@gmail.com>
2026-09-13 15:25:04 +0530

    pattern_11

---
+++ 03_patterns.md/p11.cpp
@@ -0,0 +1,27 @@
+/*
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern_11 (int n){
+
+     for(int i = 0; i < n; i++){
+        
+
+        }
+
+
+        cout << endl;
+    }
+
+int main() {
+
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    pattern_11(n);
+    return 0;
+}
